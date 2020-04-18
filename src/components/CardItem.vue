9e4770fb4b28cdde90344e95da53f928aa4c1e98
Y Siva Sai Krishna <sivasaikrishnassk@gmail.com>
2020-04-19 02:25:34 +0530

    Countries and flags are linked

---
+++ src/components/CardItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-col>
+      <b-card
+        :title="item.name"
+        :sub-title="item.name_official"
+        :img-src= "`https://flagpedia.net/data/flags/normal/${item.code2l.toLowerCase()}.png`"
+        :img-alt="item.name"
+        img-height="150"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >    
+        <b-list-group flush>
+          <b-list-group-item>latitude : {{item.latitude}}</b-list-group-item>
+          <b-list-group-item>longitude: {{item.longitude}}</b-list-group-item>
+          <b-list-group-item>ISO alpha-2: {{item.code2l}}</b-list-group-item>
+          <b-list-group-item>ISO alpha-3: {{item.code3l}}</b-list-group-item>
+        </b-list-group>
+
+        <!-- 
+        <b-button
+          v-b-tooltip.hover
+          title="Tooltip content"
+          v-b-modal="modalId"
+          variant="primary"
+          >Go somewhere</b-button
+        >
+        <b-modal :id="modalId" title="BootstrapVue">
+          <p class="my-4">Hello from modal!</p>
+        </b-modal> -->
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  computed: {
+    modalId() {
+      return `modal-${this.id}`;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <b-container>
+      <b-row align-v="center">
+        <CardItem v-for="item in display" :key="item.id" v-bind:item="item" />
+      </b-row>
+      <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      @input="paginate(currentPage)"
+    ></b-pagination>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CardItem from "../components/CardItem.vue";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    CardItem
+  },
+  data() {
+    return {
+      countries: [],
+      display:[],
+      currentPage:1,      
+      rows:1,
+      perPage:10
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/cristiroma/countries/master/data/json/countries.json"
+        )
+        .then(res => {
+          this.countries = res.data;
+          this.display =  res.data.slice(0, this.perPage);
+          this.rows = this.countries.length;
+        })
+        .catch(err => console.log(err));
+    },
+    paginate(currentPage){
+      console.log(currentPage);
+      const start = (currentPage-1)*this.perPage;
+      const end = currentPage*this.perPage;
+      this.display =  this.countries.slice(start, end);
+    }
   }
-}
+};
 </script>
