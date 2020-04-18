f3e847bbb6edaf92ef9a14bc4c2bbf3de00774c1
Y Siva Sai Krishna <sivasaikrishnassk@gmail.com>
2020-04-19 03:11:13 +0530

    rows, display from vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,62 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    countries: [],
+    display: [],
+    rows: 0
   },
   mutations: {
+    SET_COUNTRIES(state, countries) {
+      state.countries = countries;
+    },
+    SET_ROWS(state, rows) {
+      state.rows = rows
+    },
+    SET_DISPLAY(state, display) {
+      state.display = display
+    }
   },
   actions: {
+    async fetchData() {
+      return new Promise(resolve => {
+        axios
+          .get(
+            "https://raw.githubusercontent.com/cristiroma/countries/master/data/json/countries.json"
+          ).then(res => {
+            resolve(res.data);
+          })
+          .catch(err => console.log(err));
+      });
+    },
+    async fetchCountries({ dispatch, commit }) {
+      const countries = await dispatch("fetchData");
+      commit("SET_COUNTRIES", countries);
+      commit("SET_ROWS", countries.length);
+      commit("SET_DISPLAY", countries.slice(0, 10));
+      // dispatch("updatePagination", { myJson, currentPage: 1, perPage: 3 });
+      // return myJson;
+    },
+    async paginate({commit, state}, {currentPage, perPage}){
+      const start = (currentPage - 1)*perPage;
+      const end = currentPage*perPage;
+      commit("SET_DISPLAY", state.countries.slice(start, end));
+    }
+  },
+  getters: {
+    rows(state) {
+      return state.rows
+    },
+    countries(state) {
+      return state.countries
+    },
+    display(state) {
+      return state.display
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,15 +5,15 @@
         <CardItem v-for="item in display" :key="item.id" v-bind:item="item" />
       </b-row>
       <b-pagination
-      v-model="currentPage"
-      :total-rows="rows"
-      :per-page="perPage"
-      first-text="First"
-      prev-text="Prev"
-      next-text="Next"
-      last-text="Last"
-      @input="paginate(currentPage)"
-    ></b-pagination>
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        first-text="First"
+        prev-text="Prev"
+        next-text="Next"
+        last-text="Last"
+        @input="paginate(currentPage)"
+      ></b-pagination>
     </b-container>
   </div>
 </template>
@@ -21,43 +21,34 @@
 <script>
 // @ is an alias to /src
 import CardItem from "../components/CardItem.vue";
-import axios from "axios";
+import { mapGetters } from "vuex";
 
 export default {
   name: "Home",
   components: {
     CardItem
   },
+  computed: {
+    ...mapGetters(["rows", "countries", "display"])
+  },
   data() {
     return {
-      countries: [],
-      display:[],
-      currentPage:1,      
-      rows:1,
-      perPage:10
+      currentPage: 1,
+      perPage: 10
     };
   },
   mounted() {
     this.fetchData();
   },
   methods: {
-    fetchData() {
-      axios
-        .get(
-          "https://raw.githubusercontent.com/cristiroma/countries/master/data/json/countries.json"
-        )
-        .then(res => {
-          this.countries = res.data;
-          this.display =  res.data.slice(0, this.perPage);
-          this.rows = this.countries.length;
-        })
-        .catch(err => console.log(err));
+    async fetchData() {
+      await this.$store.dispatch("fetchCountries");
     },
-    paginate(currentPage){
-      console.log(currentPage);
-      const start = (currentPage-1)*this.perPage;
-      const end = currentPage*this.perPage;
-      this.display =  this.countries.slice(start, end);
+    async paginate(currentPage) {
+      await this.$store.dispatch("paginate", {
+        currentPage,
+        perPage: this.perPage
+      });
     }
   }
 };
